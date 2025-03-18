e1e7496de6ba4e489fc5da3b9d62b7a6a8584a03
Muhammad <muhammadabdullahgohar572@gmail>
2025-03-19 01:29:19 +0500

    first commit

--- chaiDjango/chai/models.py
+++ chaiDjango/chai/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
 class Chaiverity(models.Model):
     CHAI_TYPE_CHOICE = [
-    ('ML', 'MASALA'),
-    ('GR', 'GINGER'),
-    ('KL', 'KIWI'),
-    ('PL', 'PLAIN'),
-    ('EL', 'ELACHI'),
-   ]
-    name=models.CharField(max_length=20)
-    image=models.ImageField(upload_to='chais/')
-    time=models.DateTimeField(default=timezone.now)
-    type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
-    description=models.TextField(default='')
-    price=models.IntegerField(default=0)
-    
-    
+        ('MASALA', 'Masala'),
+        ('GINGER', 'Ginger'),
+        ('KIWI', 'Kiwi'),
+        ('PLAIN', 'Plain'),
+        ('ELACHI', 'Elachi'),
+    ]
+
+    name = models.CharField(max_length=50)  # Increased length
+    image = models.ImageField(upload_to='chais/', max_length=100)  # File name limit fix
+    time = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=10, choices=CHAI_TYPE_CHOICE)  # Increased length
+    description = models.TextField(default='', blank=True, null=True)
+
+    price = models.IntegerField(default=0)
+
     def __str__(self):
-          return self.name
+        return self.name
