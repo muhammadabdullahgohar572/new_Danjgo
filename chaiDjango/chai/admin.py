29c81eea359a980689a3d375c8fe778357900c7e
Muhammad <muhammadabdullahgohar572@gmail>
2025-03-21 02:14:19 +0500

    first commit

--- chaiDjango/chai/admin.py
+++ chaiDjango/chai/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Chaiverity
+from .models import Chaiverity,store,ChaiReview,chaicertificate
 
-admin.site.register(Chaiverity)
+
+class ChaiReviewInline(admin.TabularInline):
+    extra=2;
+    model=ChaiReview
+    
+    
+class ChaiverityAdmin(admin.ModelAdmin):
+    list_display=['name','type','time']
+    inlines=[ChaiReviewInline]   
+    
+class StoreAdmin(admin.ModelAdmin):
+    list_display=['name','location']
+    filter_horizontal=('chai_Chaiverity',)
+class chaicertificate_admin(admin.ModelAdmin):
+    list_display=['chai','certificate_number'] 
+         
+
+admin.site.register(Chaiverity,ChaiverityAdmin)
+admin.site.register(store,StoreAdmin)
+admin.site.register(chaicertificate,chaicertificate_admin)
 # Register your models here.
--- chaiDjango/chai/models.py
+++ chaiDjango/chai/models.py
@@ -1,22 +1,61 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Chaiverity(models.Model):
     CHAI_TYPE_CHOICE = [
-        ('MASALA', 'Masala'),
-        ('GINGER', 'Ginger'),
-        ('KIWI', 'Kiwi'),
-        ('PLAIN', 'Plain'),
-        ('ELACHI', 'Elachi'),
+        ("MASALA", "Masala"),
+        ("GINGER", "Ginger"),
+        ("KIWI", "Kiwi"),
+        ("PLAIN", "Plain"),
+        ("ELACHI", "Elachi"),
     ]
 
     name = models.CharField(max_length=50)  # Increased length
-    image = models.ImageField(upload_to='chais/', max_length=100)  # File name limit fix
+    image = models.ImageField(upload_to="chais/", max_length=100)  # File name limit fix
     time = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=10, choices=CHAI_TYPE_CHOICE)  # Increased length
-    description = models.TextField(default='', blank=True, null=True)
-
+    description = models.TextField(default="", blank=True, null=True)
     price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
+
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(
+        Chaiverity, on_delete=models.CASCADE, related_name="reviews"
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now())
+    
+    
+    def __str__(self):
+        return f'{self.user.username}reviews for {self.chai.name}'
+    
+    
+class store(models.Model):
+    name=models.CharField(max_length=100);
+    location=models.CharField(max_length=100)
+    chai_Chaiverity=models.ManyToManyField(Chaiverity,related_name='store')   
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+class chaicertificate(models.Model):
+    chai=models.OneToOneField(Chaiverity,on_delete=models.CASCADE,related_name='Certificate')
+    certificate_number=models.CharField(max_length=100);
+    issue_Date=models.DateTimeField(default=timezone.now)
+    valid_untill=models.DateTimeField()
+
+
+def __str__(self):
+    return f'certificate for {self.name.chai}'
+    
+        
+    
